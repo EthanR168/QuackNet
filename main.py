8877c0e5422a308948bd3621c901a3d4abd6faed
duckandduck <ethan.risse168@gmail.com>
2025-05-01 21:42:20 +0100

    made forward propagation, make weights and biases specialised to the activation function

---
+++ main.py
@@ -0,0 +1,72 @@
+import math, random
+
+#weights are in [number of layers][size of current layer][size of next layer]
+#biases are in [number of layers][current node in the layer]
+
+class Network:
+    def __init__(self):
+        self.layers = []
+        self.weights = []
+        self.biases = []
+
+    def createWeightsAndBiases(self):
+        #weights are in [number of layers][size of current layer][size of next layer]
+        for i in range(1, len(self.layers)):
+            currSize = self.layers[i][0]
+            lastSize = self.layers[i - 1][0]
+            actFunc = self.layers[i][1]
+
+            if(actFunc == self.relu):
+                bounds =  math.sqrt(2 / lastSize) # He initialisation
+            elif(actFunc == self.sigmoid):
+                bounds = math.sqrt(6/ (lastSize + currSize)) # Xavier initialisation
+            else:
+                bounds = 1
+                
+            currW = []
+            for _ in currSize:
+                w = []
+                for _ in lastSize:
+                    w.append(random.gauss(0, bounds)) 
+                currW.append(w)
+            self.weights.append(currW)
+
+            b = []
+            for _ in currSize:
+                b.append(0)
+            self.biases.append(b)
+
+    def relu(self, value):
+        return max(0, value)
+    
+    def sigmoid(self, value):
+        return (1 / (1 + math.exp(-value)))
+    
+    def linear(self, value): # <-- Warning very computationaly expsensive, so be wary when using
+        return value
+
+    def addLayer(self, size, activationFunction):
+        funcs = {
+            "relu": self.relu,
+            "sigmoid": self.sigmoid,
+            "linear": self.linear,
+        }
+        if(activationFunction.lower() in funcs):
+            raise ValueError(f"Activation function not made: {activationFunction.lower()}")
+        self.layers.append([size, funcs[activationFunction.lower()]])
+
+    def calculateLayerNodes(self, lastLayerNodes, lastLayerWeights, biases, currentLayer):
+        layer = []
+        for i in range(currentLayer[0]):
+            summ = biases[i]
+            for j in range(len(lastLayerNodes)):
+                summ += lastLayerNodes[j] * lastLayerWeights[j][i]
+            layer.append(currentLayer[1](summ))
+        return layer
+    
+    def forwardPropagation(self, inputData):
+        layerNodes = [inputData]
+        for i in range(1, len(self.layers)):
+            layerNodes.append(self.calculateLayerNodes(layerNodes[i - 1], self.weights[i], self.biases[i], self.layers[i]))
+        return layerNodes[len(layerNodes) - 1]
+    

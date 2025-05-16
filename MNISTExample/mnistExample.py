added backpropagation unit tests and fixed some small bugs

MNISTExample/mnistExample.py
@@ -1,21 +1,31 @@
 import numpy as np
-
-# Load the preprocessed data
-train_images = np.load('MNISTExample/train_images.npy')  # Shape: (60000, 784)
-train_labels = np.load('MNISTExample/train_labels.npy')  # Shape: (60000, 10)
-test_images = np.load('MNISTExample/test_images.npy')   # Shape: (10000, 784)
-test_labels = np.load('MNISTExample/test_labels.npy')    # Shape: (10000, 10)
-
-from neuralLibrary.main import Network
 import time
 
-n = Network()
-n.addLayer(784, "relu")
-n.addLayer(64, "relu")
-n.addLayer(10, "softmax")
-n.createWeightsAndBiases()
+# Load the preprocessed data
+train_images = np.load('MNISTExample/data/train_images.npy')  # Shape: (60000, 784)
+train_labels = np.load('MNISTExample/data/train_labels.npy')  # Shape: (60000, 10)
+#test_images = np.load('MNISTExample/data/test_images.npy')   # Shape: (10000, 784)
+#test_labels = np.load('MNISTExample/data/test_labels.npy')    # Shape: (10000, 10)
 
-print("started")
-start = time.time()
-n.train(train_images, train_labels , 1)
-print(f"took: {time.time() - start}, took on average: {(time.time() - start)/len(train_images)} seconds")
+from neuralLibrary.main import Network
+
+def run(epochs):
+    n = Network(learningRate=1)
+    n.addLayer(784, "relu")
+    n.addLayer(128, "relu")
+    n.addLayer(128, "relu")
+    n.addLayer(10, "softmax")
+
+    n.createWeightsAndBiases()
+    n.write()
+
+    for _ in range(epochs):
+        print("started")
+        start = time.time()
+
+        accuaracy = n.train(train_images[0:1], train_labels[0:1] , 1)
+        print(f"took: {(time.time() - start)} seconds, accuracy: {accuaracy}")
+    
+    n.write()
+
+run(100)

MNISTExample/mnistMainBenchmark.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 # Load the preprocessed data
-train_images = np.load('MNISTExample/train_images.npy')  # Shape: (60000, 784)
-train_labels = np.load('MNISTExample/train_labels.npy')  # Shape: (60000, 10)
-test_images = np.load('MNISTExample/test_images.npy')    # Shape: (10000, 784)
-test_labels = np.load('MNISTExample/test_labels.npy')    # Shape: (10000, 10)
+train_images = np.load('MNISTExample/data/train_images.npy')  # Shape: (60000, 784)
+train_labels = np.load('MNISTExample/data/train_labels.npy')  # Shape: (60000, 10)
+test_images = np.load('MNISTExample/data/test_images.npy')    # Shape: (10000, 784)
+test_labels = np.load('MNISTExample/data/test_labels.npy')    # Shape: (10000, 10)
 
 from neuralLibrary.main import Network
 import time
@@ -12,7 +12,8 @@ import time
 def benchmark(size):
     n = Network()
     n.addLayer(784, "relu")
-    n.addLayer(64, "relu")
+    n.addLayer(128, "relu")
+    n.addLayer(128, "relu")
     n.addLayer(10, "softmax")
     n.createWeightsAndBiases()
 

MNISTExample/saveMNISTimages.py
@@ -9,11 +9,11 @@ trainset = datasets.MNIST(root='MNISTExample/data', train=True, download=True, t
 testset = datasets.MNIST(root='MNISTExample/data', train=False, download=True, transform=transform)
 
 # Preprocess and save the data as arrays
-train_images = trainset.data.view(-1, 28*28).numpy() / 255.0  # Normalize pixel values to [0,1]
-train_labels = np.eye(10)[trainset.targets.numpy()]          # One-hot encode labels
+train_images = trainset.data.view(-1, 28*28).numpy().astype('float16') / 255.0  # Normalize pixel values to [0,1]
+train_labels = np.eye(10, dtype='float16')[trainset.targets.numpy()]          # One-hot encode labels
 
-test_images = testset.data.view(-1, 28*28).numpy() / 255.0
-test_labels = np.eye(10)[testset.targets.numpy()]
+test_images = testset.data.view(-1, 28*28).numpy().astype('float16') / 255.0
+test_labels = np.eye(10, dtype='float16')[testset.targets.numpy()]
 
 # save to disk for reuse
 np.save('MNISTExample/data/train_images.npy', train_images)

neuralLibrary/backPropgation.py
@@ -47,7 +47,9 @@ def outputLayerWeightChange(lossDerivative, activationDerivative, currentLayerNo
         lossDerivativeValue = CrossEntropyLossDerivative(currentLayerNodes, trueValues, activationDerivative)
     else:
         lossDerivativeValue = lossDerivative(currentLayerNodes, trueValues, len(currentLayerNodes))
-    if(activationDerivative == SoftMaxDerivative):
+    if(activationDerivative == SoftMaxDerivative and lossDerivative == CrossEntropyLossDerivative):
+        errorTerms = currentLayerNodes - trueValues
+    elif(activationDerivative == SoftMaxDerivative):
         softs = []
         for i in range(len(trueValues)):
             softs.append(SoftMaxDerivative(i, trueValues, currentLayerNodes, lossDerivative))
@@ -59,6 +61,7 @@ def outputLayerWeightChange(lossDerivative, activationDerivative, currentLayerNo
 
 def hiddenLayerWeightChange(pastLayerErrorTerms, pastLayerWeights, activationDerivative, currentLayerNodes, pastLayerNodes):
     errorTerms = (pastLayerErrorTerms @ pastLayerWeights.T) * activationDerivative(currentLayerNodes)
+    errorTerms = errorTerms.squeeze()
     weightGradients = np.outer(pastLayerNodes, errorTerms)
     return weightGradients, errorTerms
 
@@ -99,11 +102,13 @@ def backPropgation(layerNodes, weights, biases, trueValues, layers, lossFunction
     b, biasErrorTerms = outputLayerBiasChange(lossDerivatives[lossFunction], activationDerivatives[layers[len(layers) - 1][1]], layerNodes[len(layerNodes) - 1], trueValues)
     weightGradients = [w]
     biasGradients = [b]
+    print("hhhhhhhhhhhhhhhhhhh ", w[0][0])
     for i in range(len(layers) - 2, -1, -1):
         w, weightErrorTerms = hiddenLayerWeightChange(weightErrorTerms, weights[i], activationDerivatives[layers[i][1]], layerNodes[i], layerNodes[i + 1])
         b, biasErrorTerms = hiddenLayerBiasChange(biasErrorTerms, weights[i], activationDerivatives[layers[i][1]], layerNodes[i], layerNodes[i + 1])
         weightGradients.append(w)
         biasGradients.append(b)
+        print("hhhhhhhhhhhhhhhhhhh ", w[0][0])
     weightGradients.pop(0)
     biasGradients.pop(0)
     weightGradients.reverse()

neuralLibrary/main.py
@@ -3,9 +3,10 @@ from .activationFunctions import relu, sigmoid, tanH, linear, softMax
 from .lossFunctions import MSELossFunction, MAELossFunction, CrossEntropyLossFunction
 from .optimisers import Optimisers
 from .initialisers import Initialisers
+from .writeAndReadWeightBias import writeAndRead
 import numpy as np
 
-class Network(Optimisers, Initialisers):
+class Network(Optimisers, Initialisers, writeAndRead):
     def __init__(self, lossFunc = "MSE", learningRate = 0.01, optimisationFunc = "gd", useMomentum = False, momentumCoefficient = 0.9, momentumDecay = 0.99, useBatches = False, batchSize = 32):
         self.layers = []
         self.weights = []
@@ -67,13 +68,14 @@ class Network(Optimisers, Initialisers):
         return weightGradients, biasGradients
 
     def train(self, inputData, labels, epochs):
-        self.optimisationFunction(inputData, labels, epochs, self.weights, self.biases, self.momentumCoefficient, self.momentumDecay, self.useMomentum, self.velocityWeight, self.velocityBias, self.learningRate, self.batchSize)
-
-'''        
-n = Network()
-n.addLayer(3)
-n.addLayer(2)
-n.addLayer(1)
-n.createWeightsAndBiases()
-n.train([[0.5, 0.5, 0.5]], np.array([1]), 1)
-'''
+        correct = 0
+        print(self.weights[0][0][0])
+        nodes, self.weights, self.biases, self.velocityWeight, self.velocityBias = self.optimisationFunction(inputData, labels, epochs, self.weights, self.biases, self.momentumCoefficient, self.momentumDecay, self.useMomentum, self.velocityWeight, self.velocityBias, self.learningRate, self.batchSize)        
+        for i in range(len(nodes)):
+            nodeIndex = np.argmax(nodes[i])
+            labelIndex = np.argmax(labels[i])
+            print(nodeIndex, labelIndex)
+            if(nodeIndex == labelIndex):
+                correct += 1
+        print("eeeeeeeeeeeeeeeee ", self.weights[0][0][0])
+        return correct / len(labels)
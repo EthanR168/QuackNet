fixed bugs in cnn backpropgation

ExampleCode/CNNExample.py
@@ -3,10 +3,8 @@ from neuralLibrary.main import Network
 import numpy as np
 
 # Creating parameters for convulational layer
-inputTensor = np.random.randn(4, 4, 4) #(depth, height, width)
-kernalWeights = np.random.randn(3, 4, 2, 2)
-kernalBiases = np.random.randn(3)
-trueValues = np.array([[1]])
+inputTensor = [np.random.randn(4, 4, 4), np.random.randn(4, 4, 4)] #[(depth, height, width), (depth, height, width)]
+trueValues = np.array([[1], [1]])
 
 # Define the dense layer
 net = Network()  
@@ -15,20 +13,16 @@ net.addLayer(1)
 net.createWeightsAndBiases()
 
 # Define the CNN model
-CNN = CNNModel()
-CNN.addLayer(ConvLayer(kernalWeights.shape[2], kernalWeights, kernalBiases, len(kernalWeights), 2, padding = "1"))
+CNN = CNNModel(net)
+CNN.addLayer(ConvLayer(2, 4, 3, 2, padding = "1"))
 CNN.addLayer(ActivationLayer())
 CNN.addLayer(PoolingLayer(2, 1, "max"))
 CNN.addLayer(DenseLayer(net))
 
-allTensors = CNN.forward(inputTensor)
+# Creates weights and biases 
+CNN.createWeightsBiases()
 
-print("input tensor:")
-print(inputTensor)
-print("output tensor:")
-print(allTensors[-1])
+accuaracy, loss = CNN.train(inputTensor, trueValues, False, 1)
 
-print(f"input shape: {inputTensor.shape}")
-print(f"output shape: {allTensors[-1].shape}")
-
-weightGradients, biasGradients = CNN.backpropagation(allTensors, trueValues)
+print(f"average accauracy: {accuaracy}%")
+print(f"average loss: {loss}")

neuralLibrary/convulationalManager.py
@@ -4,11 +4,12 @@ from .activationDerivativeFunctions import ReLUDerivative
 from .convulationalOptimiser import CNNoptimiser
 import numpy as np
 
-class CNNModel():
-    def __init__(self):
+class CNNModel(CNNoptimiser):
+    def __init__(self, NeuralNetworkClass):
         self.layers = []
         self.weights = []
         self.biases = []
+        self.NeuralNetworkClass = NeuralNetworkClass
     
     def addLayer(self, layer):
         self.layers.append(layer)
@@ -25,38 +26,60 @@ class CNNModel():
         allWeightGradients = [weightGradients]
         allBiasGradients = [biasGradients]
         for i in range(len(self.layers) - 2, -1, -1):
-            if(self.layers[i] == PoolingLayer or self.layers[i] == ActivationLayer):
-                errorTerms = self.layers[i].backpropagation(errorTerms, allTensors[len(allTensors) - i])
-            elif(self.layers[i] == ConvLayer):
-                weightGradients, biasGradients, errorTerms = self.layers[i].backpropagation(errorTerms, allTensors[len(allTensors) - i])
-                allWeightGradients.append(weightGradients)
-                allBiasGradients.append(biasGradients)
+            if(type(self.layers[i]) == PoolingLayer or type(self.layers[i]) == ActivationLayer):
+                errorTerms = self.layers[i].backpropagation(errorTerms, allTensors[i])
+            elif(type(self.layers[i]) == ConvLayer):
+                weightGradients, biasGradients, errorTerms = self.layers[i].backpropagation(errorTerms, allTensors[i])
+                allWeightGradients.insert(0, weightGradients)
+                allBiasGradients.insert(0, biasGradients)
         
         return allWeightGradients, allBiasGradients
     
-    def optimser(self, inputData, labels, weights, biases, batchSize, alpha, beta1, beta2, epsilon):
-        CNNoptimiser.AdamsOptimiser(self, inputData, labels, weights, biases, batchSize, alpha, beta1, beta2, epsilon)
+    def optimser(self, inputData, labels, useBatches, weights, biases, batchSize, alpha, beta1, beta2, epsilon):
+        if(useBatches == True):
+            return CNNoptimiser.AdamsOptimiserWithBatches(self, inputData, labels, weights, biases, batchSize, alpha, beta1, beta2, epsilon)
+        else:
+            return CNNoptimiser.AdamsOptimiserWithoutBatches(self, inputData, labels, weights, biases, alpha, beta1, beta2, epsilon)
     
-    def train(self, inputData, labels, batchSize, alpha, beta1, beta2, epsilon):
+    def train(self, inputData, labels, useBatches, batchSize, alpha = 0.001, beta1 = 0.9, beta2 = 0.999, epsilon = 1e-8):
         correct, totalLoss = 0, 0
         
-        nodes, self.weights, self.biases, self.velocityWeight, self.velocityBias = self.optimser(self, inputData, labels, self.weights, self.biases, batchSize, alpha, beta1, beta2, epsilon)        
+        nodes, self.weights, self.biases = self.optimser(inputData, labels, useBatches, self.weights, self.biases, batchSize, alpha, beta1, beta2, epsilon)        
         
         lastLayer = len(nodes[0]) - 1
         for i in range(len(nodes)): 
-            totalLoss += self.lossFunction(nodes[i][lastLayer], labels[i])
+            totalLoss += self.NeuralNetworkClass.lossFunction(nodes[i][lastLayer], labels[i])
             nodeIndex = np.argmax(nodes[i][lastLayer])
             labelIndex = np.argmax(labels[i])
             if(nodeIndex == labelIndex):
                 correct += 1
-        return correct / len(labels), totalLoss / len(labels)
+        return 100 * (correct / len(labels)), totalLoss / len(labels)
+    
+    def createWeightsBiases(self):
+        for i in range(len(self.layers)):
+            if(type(self.layers[i]) == ConvLayer):
+                kernalSize = self.layers[i].kernalSize
+                numKernals = self.layers[i].numKernals
+                depth = self.layers[i].depth
+
+                bounds =  np.sqrt(2 / kernalSize) # He initialisation
+
+                self.weights.append(np.random.normal(0, bounds, size=(numKernals, depth, kernalSize, kernalSize)))
+                self.biases.append(np.zeros((numKernals)))
+
+                self.layers[i].kernalWeights = self.weights[-1]
+                self.layers[i].kernalBiases = self.biases[-1]
+            elif(type(self.layers[i]) == DenseLayer):
+                self.weights.append(self.layers[i].NeuralNetworkClass.weights)
+                self.biases.append(self.layers[i].NeuralNetworkClass.biases)
 
 class ConvLayer(ConvulationalNetwork, CNNbackpropagation):
-    def __init__(self, kernalSize, kernalWeights, kernalBiases, numKernals, stride, padding = "no"):
+    def __init__(self, kernalSize, depth, numKernals, stride, padding = "no"):
         self.kernalSize = kernalSize
         self.numKernals = numKernals
-        self.kernalWeights = kernalWeights
-        self.kernalBiases = kernalBiases
+        self.kernalWeights = []
+        self.kernalBiases = []
+        self.depth = depth
         self.stride = stride
         self.padding = padding
         if(padding.lower() == "no"):
@@ -91,20 +114,25 @@ class PoolingLayer(CNNbackpropagation):
 class DenseLayer: # basically a fancy neural network
     def __init__(self, NeuralNetworkClass):
         self.NeuralNetworkClass = NeuralNetworkClass
+        self.orignalShape = 0
     
     def forward(self, inputTensor):
+        self.orignalShape = inputTensor.shape
         inputArray = ConvulationalNetwork.flatternTensor(self, inputTensor)
         self.layerNodes = self.NeuralNetworkClass.forwardPropagation(inputArray)
         return self.layerNodes[-1]
     
     def backpropagation(self, trueValues): #return weigtGradients, biasGradients, errorTerms
-        return self.NeuralNetworkClass.backPropgation(
+        weightGradients, biasGradients, errorTerms = self.NeuralNetworkClass.backPropgation(
             self.layerNodes, 
             self.NeuralNetworkClass.weights,
             self.NeuralNetworkClass.biases,
             trueValues,
             True
         )
+        errorTerms = np.array(self.NeuralNetworkClass.weights).T @ errorTerms 
+        errorTerms = errorTerms.reshape(self.orignalShape)
+        return weightGradients, biasGradients, errorTerms
 
 class ActivationLayer: # basically aplies an activation function over the whole network (eg. leaky relu)
     def forward(self, inputTensor):

neuralLibrary/convulationalOptimiser.py
@@ -1,20 +1,34 @@
-from .convulationalManager import CNNModel
 import numpy as np
 
 class CNNoptimiser:
-    def AdamsOptimiser(self, inputData, labels, weights, biases, batchSize, alpha = 0.001, beta1 = 0.9, beta2 = 0.999, epsilon = 1e-8):
+    def AdamsOptimiserWithBatches(self, inputData, labels, weights, biases, batchSize, alpha, beta1, beta2, epsilon):
         firstMomentWeight, firstMomentBias, secondMomentWeight, secondMomentBias = self.initialiseMoment(weights, biases)
         weightGradients, biasGradients = self.initialiseGradients(weights, biases)
+        allNodes = []
         for i in range(0, len(inputData), batchSize):
             batchData = inputData[i:i+batchSize]
             batchLabels = labels[i:i+batchSize]
-            weightGradients, biasGradients = self.initialiseGradients(weights, biases)
             for j in range(len(batchData)):
-                layerNodes = CNNModel.forward(self, batchData[j])
-                w, b = CNNModel.backpropagation(self, layerNodes, batchLabels[j])
+                layerNodes = self.forward(batchData[j])
+                allNodes.append(layerNodes)
+                w, b = self.backpropagation(layerNodes, batchLabels[j])
                 weightGradients, biasGradients = self.addGradients(weightGradients, biasGradients, w, b)
             weights, biases, firstMomentWeight, firstMomentBias, secondMomentWeight, secondMomentBias = self.Adams(weightGradients, biasGradients, weights, biases, i + 1, firstMomentWeight, firstMomentBias, secondMomentWeight, secondMomentBias, alpha, beta1, beta2, epsilon)
-        return weights, biases
+            weightGradients, biasGradients = self.initialiseGradients(weights, biases)
+        return allNodes, weights, biases
+
+    def AdamsOptimiserWithoutBatches(self, inputData, labels, weights, biases, alpha, beta1, beta2, epsilon):
+        firstMomentWeight, firstMomentBias, secondMomentWeight, secondMomentBias = self.initialiseMoment(weights, biases)
+        weightGradients, biasGradients = self.initialiseGradients(weights, biases)
+        allNodes = []
+        for i in range(len(inputData)):
+            layerNodes = self.forward(inputData[i])
+            allNodes.append(layerNodes)
+            w, b = self.backpropagation(layerNodes, labels[i])
+            weightGradients, biasGradients = self.addGradients(weightGradients, biasGradients, w, b)
+            weights, biases, firstMomentWeight, firstMomentBias, secondMomentWeight, secondMomentBias = self.Adams(weightGradients, biasGradients, weights, biases, i + 1, firstMomentWeight, firstMomentBias, secondMomentWeight, secondMomentBias, alpha, beta1, beta2, epsilon)
+            weightGradients, biasGradients = self.initialiseGradients(weights, biases)
+        return allNodes, weights, biases
 
     def Adams(self, weightGradients, biasGradients, weights, biases, timeStamp, firstMomentWeight, firstMomentBias, secondMomentWeight, secondMomentBias, alpha, beta1, beta2, epsilon):
         for i in range(len(weights)):
@@ -45,6 +59,9 @@ class CNNoptimiser:
         return weightGradients, biasGradients
 
     def addGradients(self, weightGradients, biasGradients, w, b):
+        print(np.array(weightGradients[0]).shape)
+        print(np.array(weightGradients[1]).shape)
+        print(np.array(w[0]).shape)
         for i in range(len(weightGradients)):
             weightGradients[i] += w[i]
             weightGradients[i] = np.clip(weightGradients[i], -1, 1)
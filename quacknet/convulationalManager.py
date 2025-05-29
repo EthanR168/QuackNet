corrected gradient calculation in adams optimiser

quacknet/convulationalManager.py
@@ -73,16 +73,33 @@ class CNNModel(CNNoptimiser):
                 self.weights.append(self.layers[i].NeuralNetworkClass.weights)
                 self.biases.append(self.layers[i].NeuralNetworkClass.biases)
 
-    def saveModel(self, weights, biases, filename = "modelWeights.npz"):
-        weights = np.array(weights, dtype=object)
-        biases = np.array(biases, dtype=object)
-        np.savez_compressed(filename, weights = weights, biases = biases, allow_pickle = True)
+    def saveModel(self, NNweights, NNbiases, CNNweights, CNNbiases, filename = "modelWeights.npz"):
+        CNNweights = np.array(CNNweights, dtype=object)
+        CNNbiases = np.array(CNNbiases, dtype=object)
+        NNweights = np.array(NNweights, dtype=object)
+        NNbiases = np.array(NNbiases, dtype=object)
+        np.savez_compressed(filename, CNNweights = CNNweights, CNNbiases = CNNbiases, NNweights = NNweights, NNbiases = NNbiases, allow_pickle = True)
 
-    def loadModel(self, filename = "modelWeights.npz"):
+    def loadModel(self, neuralNetwork, filename = "modelWeights.npz"):
         data = np.load(filename, allow_pickle = True)
-        weights = data["weights"]
-        biases = data["biases"]
-        return weights, biases
+        CNNweights = data["CNNweights"]
+        CNNbiases = data["CNNbiases"]
+        NNweights = data["NNweights"]
+        NNbiases = data["NNbiases"]
+
+        self.layers[-1].NeuralNetworkClass.weights = NNweights
+        self.layers[-1].NeuralNetworkClass.biases = NNbiases
+        neuralNetwork.weights = NNweights
+        neuralNetwork.biases = NNbiases
+        self.weights = CNNweights
+        self.biases = CNNbiases
+
+        currWeightIndex = 0
+        for i in range(len(self.layers)):
+            if(type(self.layers[i]) == ConvLayer):
+                self.layers[i].kernalWeights = CNNweights[currWeightIndex]
+                self.layers[i].kernalBiases = CNNbiases[currWeightIndex]
+                currWeightIndex += 1
 
 class ConvLayer(ConvulationalNetwork, CNNbackpropagation):
     def __init__(self, kernalSize, depth, numKernals, stride, padding = "no"):

quacknet/convulationalOptimiser.py
@@ -13,9 +13,10 @@ class CNNoptimiser:
                 layerNodes = self.forward(batchData[j])
                 allNodes.append(layerNodes)
                 w, b = self.backpropagation(layerNodes, batchLabels[j])
-                weightGradients, biasGradients = self.addGradients(weightGradients, biasGradients, w, b)
+                weightGradients, biasGradients = self.addGradients(batchSize, weightGradients, biasGradients, w, b)
             weights, biases, firstMomentWeight, firstMomentBias, secondMomentWeight, secondMomentBias = self._Adams(weightGradients, biasGradients, weights, biases, i + 1, firstMomentWeight, firstMomentBias, secondMomentWeight, secondMomentBias, alpha, beta1, beta2, epsilon)
             weightGradients, biasGradients = self.initialiseGradients(weights, biases)
+            print(f"finished batch: {(i // batchSize) + 1}/{len(inputData) // batchSize}")
         return allNodes, weights, biases
 
     def AdamsOptimiserWithoutBatches(self, inputData, labels, weights, biases, alpha, beta1, beta2, epsilon):
@@ -27,7 +28,7 @@ class CNNoptimiser:
             layerNodes = self.forward(inputData[i])
             allNodes.append(layerNodes)
             w, b = self.backpropagation(layerNodes, labels[i])
-            weightGradients, biasGradients = self.addGradients(weightGradients, biasGradients, w, b)
+            weightGradients, biasGradients = self.addGradients(1, weightGradients, biasGradients, w, b)
             weights, biases, firstMomentWeight, firstMomentBias, secondMomentWeight, secondMomentBias = self._Adams(weightGradients, biasGradients, weights, biases, i + 1, firstMomentWeight, firstMomentBias, secondMomentWeight, secondMomentBias, alpha, beta1, beta2, epsilon)
             weightGradients, biasGradients = self.initialiseGradients(weights, biases)
         return allNodes, weights, biases
@@ -68,15 +69,15 @@ class CNNoptimiser:
             biasGradients.append(b)
         return weightGradients, biasGradients
 
-    def addGradients(self, weightGradients, biasGradients, w, b):
+    def addGradients(self, batchSize, weightGradients, biasGradients, w, b):
         for i in range(len(weightGradients)):
             for j in range(len(weightGradients[i])):
-                weightGradients[i][j] += np.array(w[i][j])
+                weightGradients[i][j] += np.array(w[i][j]) / batchSize
             #weightGradients[i] = np.clip(weightGradients[i], -1, 1)
 
         for i in range(len(biasGradients)):
             for j in range(len(biasGradients[i])):
-                biasGradients[i][j] += np.array(b[i][j])
+                biasGradients[i][j] += np.array(b[i][j]) / batchSize
             #biasGradients[i] = np.clip(biasGradients[i], -1, 1)
         return weightGradients, biasGradients
 